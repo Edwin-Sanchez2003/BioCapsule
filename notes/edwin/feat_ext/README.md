6587971d8f71fc14a66b5fec299d4eef8284617b
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-07-16 15:23:11 -0400

    code reads in config file and json template

--- notes/edwin/feat_ext/README.md
+++ notes/edwin/feat_ext/README.md
@@ -16,7 +16,7 @@ This information is stored once in each JSON file, rather than stored in with ea
 * **subject_ID:** *(string)* The ID of the subject in the video.
 * **session_ID:** *(int)* The ID of the session the video belongs to. A number from 1 to 12, inclusive.
 * **phase:** *(int)* The phase of the test the video belongs to (either phase 1 or phase 2).
-* **device:** *(string)* The device the video was recorded on. Either *phone* or *laptop*.
+* **device:** *(string)* The device the video was recorded on. Either *phone/mobile* or *laptop*.
 * **pre_proc_model:** *(string)* The model used during preprocessing.
 * **feat_ext_model:** *(string)* The model used to perform the feature extraction.
 * **feat_vect_length:** *(int)* The length of the resulting feature vectors - based on the selected model for feature_extraction.
@@ -31,6 +31,29 @@ The data below is collected for each frame of the video:
 * **feature_vector:** The resulting feature vector (of the person) derived from the frame, after preprocessing and feature extraction.
 
 ## Config File Format
-This file is a JSON file passed in as a parameter to the program and specifies certain properties of the program and the extraction process. The format is specified below:
+This file is a JSON file passed in as a parameter to the program and specifies certain properties of the program and the extraction process. JSON files are named after the subjectID, sessionID, and device that the video came from. The format is specified below:
 
-* ****
+* **input_path:** *(string)* The directory containing a list of videos OR the path to a single video.
+* **output_dir:** *(string)* The directory to store the file. File names are generated based on subjectID, sessionID, and the device that the video came from.
+* **json_template_path:** The path to the JSON_DATA_TEMPLATE.json file, which is used as a starting point for the json file to be stored.
+* **pre_proc_model:** *(string)* The model to use for preprocessing. *Limited to options set in code.
+* **feat_ext_model:** *(string)* The model to use for feature extraction. *Limited to options set in code.
+* **max_num_processes:** *(int)* The number of processes to run to maximize CPU utilization. Defaults to 1.
+* **apply_partial_occlusion:** *(boolean)* Whether or not to generate black squares randomly on the input frames to make detection harder. *Later need to add parameters here for settings on partial occlusion.* 
+* **ignore_pre_sets**: *(boolean)* Tells the program whether or not it should ignore the presets when generating the feature vector JSON files.
+* **pre_sets:** *(dict)* Describes other details that will be stored in the output json file. Details below.
+
+### Pre Sets
+The pre-sets section in the config file specify important details about the videos that will be stored with the generated JSON file. This includes:
+
+* **location**
+* **subject_ID**
+* **session_ID**
+* **phase**
+* **device**
+
+*Descriptions of these pre-sets are the same as described in the JSON format Section of this document.*
+
+These will need to be specified for the program to run. This is to document important details about the video, specific to the MOBIO dataset.
+
+However, if you wish to ingore presets, you can do so with the **ignore_presets** parameter in the config file. These details will be excluded from the generated JSON files.
--- notes/edwin/feat_ext/generate_feature_vectors.py
+++ notes/edwin/feat_ext/generate_feature_vectors.py
@@ -11,13 +11,93 @@
 """
 
 # Imports
+import os # used for opening & reading files.
 import json # used for storing the feature vector files.
-import multiprocessing
 
+import multiprocessing # for speeding up processing.
+
+import argparse # used for passing args to this file.
+parser = argparse.ArgumentParser(
+    prog="Generate Feature Vectors",
+    description="This file is designed to generate feature vectors from videos in the MOBIO dataset and store them in JSON files with their corresponding metadata."
+) # end parser init
+
+# add arguments for file
+parser.add_argument("-c", "--config_file_path", help="The path to the configuration JSON file.")
+
+# parse for arguments
+args = parser.parse_args()
+
+print("I ran")
+# runs when this file is called directly
 def main():
+    """
+    performs extraction on one video.
+    stores all data collected into a single json file.
+    used as the job for a single process.
+
+        load config file and parameters
+        decide how many processes to create
+            load preprocessing model
+            load in feat ext model
+            load a video
+            bust into frames
+                pass each frame to models & get feature vector
+                store data into dict
+            store meta data into dict
+            store dict into json file on disk
+    """
+
     # load the config file detailing parameters
-    # for feature extraction
-    pass
+    config_data, template_data = load_configurations(config_file_path=args.config_file_path)
+    
+    # decide how many processes to generate
+    max_num_processes = 1
+    if config_data["is_file"] == False:
+        max_num_processes = config_data["max_num_processes"]
+
+    
+
+
+
+# loads the config file params into an easy to read format
+# returns the config dict and a starting point for the output JSON file
+def load_configurations(config_file_path:str)-> "tuple[dict,dict]":
+    # open JSON into a python dict
+    config_data = load_json_file(file_path=config_file_path)
+    
+    # create starting dict formatted to store data
+    data = load_json_file(config_data["json_template_path"])
+
+    # check if we should ignore the pre-sets
+    if config_data["ignore_pre_sets"]:
+        data["location"] = None
+        data["subject_ID"] = None
+        data["session_ID"] = None
+        data["phase"] = None
+        data["device"] = None
+    else:
+        data["location"] = config_data["pre_sets"]["location"]
+        data["subject_ID"] = config_data["pre_sets"]["subject_ID"]
+        data["session_ID"] = config_data["pre_sets"]["session_ID"]
+        data["phase"] = config_data["pre_sets"]["phase"]
+        data["device"] = config_data["pre_sets"]["device"]
+
+    # check if input path is a directory or a single video
+    # store for later decisions
+    config_data["is_file"] = os.path.isfile(config_data["input_path"])
+
+    # set other meta data details
+    data["pre_proc_model"] = config_data["pre_proc_model"]
+    data["feat_ext_model"] = config_data["feat_ext_model"]
+    data["pre_proc_model"] = config_data["pre_proc_model"]
+    data["pre_proc_model"] = config_data["pre_proc_model"]
+
+    # create folder for output files, if it doesn't exist already
+    if os.path.exists(config_data["output_dir"]) == False:
+        os.makedirs(config_data["output_dir"])
+
+    return config_data, data
 
 
 # creates processes to speed up the feature extraction process.
@@ -25,21 +105,6 @@ def main():
 # of data we can have in memory during execution without crashing.
 def generate_processes(num_processes):
     pass
-
-
-# performs extraction on one video.
-# stores all data collected into a single json file.
-# used as the job for a single process.
-def extract_from_video():
-    pass
-    # load preprocessing model
-    # load in feat ext model
-    # load a video
-    # bust into frames
-        # pass each frame to models & get feature vector
-        # store data into dict
-    # store meta data into dict
-    # store dict into json file on disk
 
 
 # tests data from a video to make sure its useable
@@ -77,3 +142,7 @@ def write_to_json(file_path:str, data:dict)-> None:
 def load_json_file(file_path:str)-> dict:
     with open(file_path, "r") as file:
         return json.load(file)
+
+
+if __name__ == "__main__":
+    main()
