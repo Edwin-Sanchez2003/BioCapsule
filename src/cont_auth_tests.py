25098d83d75a761330892d985f67b87905713919
= <=>
2023-07-27 11:06:06 -0400

    adding final touches

--- src/cont_auth_tests.py
+++ src/cont_auth_tests.py
@@ -81,13 +81,10 @@ from sklearn.metrics import confusion_matrix
 from test_enums import *
 from data_tools import *
 
-# Params #
-model_type = Model_Type.ARCFACE
-
 # load rs features for later bc use
 rs_data = load_json_gz_file(
     file_path="./MOBIO_extracted/one_sec_intervals/XX_removed_from_exp_XX/f210/unis_laptop_1_f210_01.json.gz")
-ref_subj_feat_vect = rs_data["frame_data"][0][model_type]
+ref_subj_feat_vect = rs_data["frame_data"][0][MODEL_TYPE]
 
 # locations in MOBIO dataset
 MOBIO_LOCATIONS = [
@@ -101,7 +98,14 @@ MOBIO_LOCATIONS = [
 
  # Params #
 BASE_DIR = "./MOBIO_extracted/one_sec_intervals/"
-
+OUT_DIR = "../MOBIO_extracted/test_results/"
+T_INTERVAL = 10
+USE_BC = True
+MODEL_TYPE = Model_Type.ARCFACE
+PLATFORM = Platform.MULTI
+WINDOW_SIZE = 1
+CLASSIFIER_TYPE = Classifier.LOGISTIC_REGRESSION
+USE_K_FOLD = False
 
 def main():
     """
@@ -142,6 +146,7 @@ def main():
         input_dirs.append(os.path.join(BASE_DIR, loc_path))
 
     # loop over input dirs
+    print("Retrieving Subject Data...")
     subjects_data = []
     for input_dir in input_dirs:
         # loop over participants
@@ -149,32 +154,50 @@ def main():
             # get out the subject data for every subject
             # and load the data into memory.
             # only the data we need to run the tests
+            print(f"Retrieving Subject {part_id}")
             subject_data = get_subj_data(
                 subj_dir=os.path.join(input_dir, part_id),
                 platform=Platform.MULTI,
-                t_interval=10,
+                t_interval=T_INTERVAL,
                 model_type=Model_Type.ARCFACE
             ) # end get_subj_data call
             subjects_data.append(subject_data)
+            print("Data Retrieved!")
         # end loop over participants in a location
     # end loop over all directories to search through for data
-    
+
     # run a test generate a file containing
     # data on the test. include test params
+    print("Running Test with the Following Params:")
+    print(f"USE_BC: {USE_BC}")
+    print(f"MODEL_TYPE: {MODEL_TYPE}")
+    print(f"PLATFORM: {PLATFORM}")
+    print(f"USE_BC: {USE_BC}")
+    print(f"USE_BC: {USE_BC}")
+    print(f"USE_BC: {USE_BC}")
+    print(f"USE_BC: {USE_BC}")
+    print(f"USE_BC: {USE_BC}")
+    
+    # print out all parameters set by the user here.
     out_data = run_test(
         subjects_data=subjects_data,
-        use_bc=False,
-        window_size=1,
-        classifier_type=Classifier.LOGISTIC_REGRESSION,
-        use_k_fold=False
+        use_bc=USE_BC,
+        window_size=WINDOW_SIZE,
+        classifier_type=CLASSIFIER_TYPE,
+        use_k_fold=USE_K_FOLD
     ) # end run_test
+
+    # store out data in a file
+    out_file_name = f"{USE_BC}_{MODEL_TYPE}_{PLATFORM}.json.gz"
+    out_file_path = os.path.join(OUT_DIR, out_file_name)
+    write_to_json_gz(out_file_path, out_data)
 # end main
 
 
 # run a test for a single participant,
 # given the input params
 # store the data generated for each invididual test for later
-def run_test(subjects_data:"list[dict]",
+def run_test(subjects_data:"list[dict]", # list of all user data split into train, val, and test
              use_bc:bool,
              window_size:int,
              classifier_type:Classifier,
@@ -183,6 +206,8 @@ def run_test(subjects_data:"list[dict]",
     # the whole subjects_data array will be used in every iteration,
     # but the classifier & test will be done with respect to the current
     # subject data dict being iterated over by the for loop
+    g_tp, g_fp, g_tn, g_fn = 0 # global tp, fp, tn, fn
+    pos_subj_results = []
     for subj_index, subject_data in enumerate(subjects_data):
         # the id of the current subject (participant id)
         subj_id = subjects_data["subject_ID"]
@@ -192,10 +217,33 @@ def run_test(subjects_data:"list[dict]",
         val_pos_samples = subject_data["train_samples"]["val_split_samples"]
 
         # extract out the train & val data from every other subject
+        train_neg_samples = []
+        val_neg_samples = []
+        for neg_subj_index, neg_subj_data in enumerate(subjects_data):
+            if neg_subj_index != subj_index:
+                train_neg_samples.extend(neg_subj_data["train_samples"]["train_split_samples"])
+                val_neg_samples.extend(neg_subj_data["train_samples"]["val_split_samples"])
+            # end if
+        # end for over train & val data collection
 
         # create labels for train & val data
-
         # combine all data together
+        train_samples, train_labels = combine_samples(train_neg_samples, train_pos_samples)
+        val_samples, val_labels = combine_samples(val_neg_samples, val_pos_samples)
+
+        # check if we should use bc
+        bc_gen = bc.BioCapsuleGenerator()
+        if use_bc:
+            train_samples = apply_bc_scheme(
+                bc_gen=bc_gen,
+                samples=train_samples,
+                reference_subject=ref_subj_feat_vect
+            ) # end apply_bc_scheme
+            val_samples = apply_bc_scheme(
+                bc_gen=bc_gen,
+                samples=val_samples,
+                reference_subject=ref_subj_feat_vect
+            ) # end apply_bc_scheme
 
         # train classifier for this subject
         # data is shuffled with a constant 'random' number
@@ -206,23 +254,185 @@ def run_test(subjects_data:"list[dict]",
         # store classifier???
 
         # perform threshold tuning using validation set
-        threshold_tuning()
+        threshold_data = threshold_tuning(classifier=classifier,
+                         val_samples=val_samples,
+                         val_labels=val_labels
+        ) # end threhold tuning
         
         # iterate over all subjects, using their test samples
-        this_subj_test_results = []
+        this_subj_test_results = None
         all_other_subj_test_results = []
+        s_tp, s_fp, s_tn, s_fn = 0 # per-classifier-subject tp, fp, tn, fn
         for test_subj_index, test_subj_data in enumerate(subjects_data):
+            # get a performance on each session using the threshold
+            subj_results = {
+                "subj_id_train": subj_id,
+                "subj_id_test": test_subj_data["subject_ID"],
+                "threshold_tuning_data": threshold_data,
+                "sess_results": []
+            } # subject results and metadata
+            tp, fp, tn, fn = 0
             for session in test_subj_data["test_samples"]:
-                session_test()
-                if subj_index == test_subj_index:
-                    pass
-                else:
-                    pass
-            # end loop over a single session for testing
+                # results from a single session from a single user
+                results = session_test(
+                    classifier=classifier,
+                    threshold=threshold_data["threshold"],
+                    bc_gen=bc_gen,
+                    use_bc=use_bc,
+                    test_samples=session["features"],
+                    test_labels=[0]*len(session["features"])
+                ) # end test for single session
+                tp += results["tp"]
+                fp += results["fp"]
+                tn += results["tn"]
+                fn += results["fn"]
+                s_tp += results["tp"]
+                s_fp += results["fp"]
+                s_tn += results["tn"]
+                s_fn += results["fn"]
+                g_tp += results["tp"]
+                g_fp += results["fp"]
+                g_tn += results["tn"]
+                g_fn += results["fn"]
+
+                subj_results["sess_results"].append(results)
+            # end loop over sessions for testing
+
+            # get per-test-subject results
+            subj_results["tp"] = tp
+            subj_results["fp"] = fp
+            subj_results["tn"] = tn
+            subj_results["fn"] = fn
+            subj_results["far"] = get_far(fp=fp, tn=tn)
+            subj_results["frr"] = get_frr(fn=fn, tp=tp)
+
+            # where to put test results
+            if subj_index == test_subj_index:
+                this_subj_test_results = subj_results
+            else:
+                all_other_subj_test_results.append(subj_results)
         # end loop over all other subjects for testing
+        # per-classifier-subject results
+        pos_subj_result = {
+            "subj_id": subj_id,
+            "pos_subj_results": this_subj_test_results,
+            "neg_subjs_restults": all_other_subj_test_results,
+            "tp": s_tp,
+            "fp": s_fp,
+            "tn": s_tn,
+            "fn": s_fn,
+            "far": get_far(fp=s_fp, tn=s_tn),
+            "frr": get_frr(fn=s_fn, tp=s_tp)
+        } # end pos_subj_results
+        pos_subj_results.append(pos_subj_result)
     # end loop focusing on each subject as the positive case
-    # store overall test performance data into file
+
+    # get final results over every subject's data
+    all_subj_results = {
+        "use_bc": use_bc,
+        "use_k_fold": use_k_fold,
+        "classifier": classifier_type,
+        "window_size": window_size,
+        "tp": g_tp,
+        "fp": g_fp,
+        "tn": g_tn,
+        "fn": g_fn,
+        "far": get_far(fp=g_fp, tn=g_tn),
+        "frr": get_frr(fn=g_fn, tp=g_tp),
+        "pos_subj_results": pos_subj_results
+    } # end results for all subjects' performance
+
+    # return data
+    return all_subj_results
 # end run_test
+
+
+def get_far(fp, tn):
+    return fp / (fp + tn)
+
+def get_frr(fn, tp):
+    return fn / (fn + tp)
+
+# return the threshold between pos and neg samples
+# that reaches an eer rate. returns the f
+def threshold_tuning(classifier:LogisticRegression,
+                     val_samples:"list[list[float]]",
+                     val_labels:"list[int]",
+                     start:int=1,
+                     stop:int=100,
+                     precision:float=0.01)-> "dict":
+    # run classifier on validation samples to get probability scores
+    preds = classifier.predict_proba(val_samples)
+    # loop over thresholds
+    for i in range(start, stop):
+        thresh = float(i*precision)
+        tp, fp, tn, fn = get_tp_fp_tn_fn(
+            thresh=thresh,
+            gt_labels=val_labels,
+            preds=preds
+        ) # end get_tp_fp_tn_fn
+        
+        #far
+        far = get_far(fp=fp, tn=tn)
+        #frr
+        frr = get_frr(fn=fn, tp=tp)
+        if is_equal_error_rate(far=far, frr=frr):
+            return {
+                "tp": tp,
+                "fp": fp,
+                "tn": tn,
+                "fn": fn,
+                "far": far,
+                "frr": frr,
+                "threshold": thresh
+            } # return threshold data dict
+    # end loop over thresholds to test
+    return {
+        "tp": None,
+        "fp": None,
+        "tn": None,
+        "fn": None,
+        "far": None,
+        "frr": None,
+        "threshold": 0.5
+    } # return threshold data dict # if eer isn't achieved...
+# threshold tuning
+
+
+# get tp, fp, tn, fn from pred probabilities and ground truth
+def get_tp_fp_tn_fn(thresh:float,
+                    gt_labels:"list[int]",
+                    preds:"list[tuple[int, int]]"
+                    )->"tuple[int, int, int, int]":
+    tp, fp, tn, fn = 0
+    for gt_label, pred_prob in zip(gt_labels, preds):
+        # check the positive class probability
+        cls = 0
+        if pred_prob[1] > thresh:
+            cls = 1
+        
+        # check if the predicted label is a tp, fp, tn or fn
+        if gt_label == 1:
+            if cls == 1:
+                tp += 1
+            else: # cls = 0
+                fn += 1
+        else: # gt_label = 0
+            if cls == 0:
+                tn += 1
+            else: # cls = 0
+                fp += 1
+        # end if deciding tp, fp, tn, fn
+    # end loop over ground truth & predicted labels
+    return tp, fp, tn, fn
+
+
+# checks if far & frr are equal, with a given precision
+def is_equal_error_rate(far:int, frr:int, precision:float=0.01)-> bool:
+    if far <= frr:
+        if (far+precision) >= frr:
+            return True
+    return False
 
 
 # applies the bc scheme, if applicable
@@ -230,20 +440,23 @@ def apply_bc_scheme(bc_gen:bc.BioCapsuleGenerator,
                     samples:"list[list[float]]",
                     reference_subject:"list[float]"
                     )-> "list[list[float]]":
-    pass
+    bc_samples = []
+    for sample in samples:
+        bc_samples.append(bc_gen.biocapsule(sample, reference_subject))
+    return bc_samples
 
 
 # session test - does a test over a single session &
 # reports the performance metrics
-def session_test():
+def session_test(classifier:LogisticRegression,
+                 threshold:float,
+                 bc_gen:bc.BioCapsuleGenerator,
+                 use_bc:bool,
+                 test_samples:"list[list[float]]",
+                 test_labels:"list[int]"
+                 )->dict:
     # apply biocapsule, if applicable
     if use_bc == True:
-        bc_gen = bc.BioCapsuleGenerator()
-        train_samples = apply_bc_scheme(
-            bc_gen=bc_gen,
-            samples=train_samples,
-            reference_subject=ref_subj_feat_vect
-        ) # end apply_bc_scheme to train_samples
         test_samples = apply_bc_scheme(
             bc_gen=bc_gen,
             samples=test_samples,
@@ -251,28 +464,44 @@ def session_test():
         ) # end apply_bc_scheme to train_samples
 
     # run classifier on test data
-    mean_acc = classifier.score(test_samples, test_labels)
+    preds = classifier.predict_proba(test_samples)
     # get confusion matrix to extract data
-    pred_test_labels = classifier.predict(test_samples)
-    conf_matrix = confusion_matrix(test_labels, pred_test_labels)
-    tn, fp, fn, tp = conf_matrix.ravel()
-    #far
-    far = fp / (fp + fn)
-    #frr
-    frr = fn / (tp + tn)
-    #eer
+    tp, fp, tn, fn = get_tp_fp_tn_fn(
+        thresh=threshold,
+        gt_labels=test_labels,
+        preds=preds
+    ) # end get_tp_fp_tn_fn
     # generate performance metrics
-    # store data for single subject into file
+    #far
+    far = get_far(fp=fp, tn=tn)
+    #frr
+    frr = get_frr(fn=fn, tp=tp)
+    # store data for single subject into dict
+    results = {
+        "tp": tp,
+        "fp": fp,
+        "tn": tn,
+        "fn": fn,
+        "far": far,
+        "frr": frr,
+        "threshold":threshold
+    } # end results
+    return results
 
 
 # creates a list containing all of one type of sample
+# uses shortest list to decide which samlpes are posit
 def combine_samples(group_A:list, # negative samples list
                     group_B:list, # positive samples list
-                    )-> "tuple[list[list[float]], list[int]]":
+                    group_A_neg:bool=True)-> "tuple[list[list[float]], list[int]]":
     len_A = len(group_A)
     len_B = len(group_B)
-    labels_A = [0]*len_A
-    labels_B = [1]*len_B
+    if group_A_neg:
+        labels_A = [0]*len_A
+        labels_B = [1]*len_B
+    else:
+        labels_A = [1]*len_A
+        labels_B = [0]*len_B
     labels = labels_A.extend(labels_B)
     samples = group_A.extend(group_B)
     return (samples, labels)
