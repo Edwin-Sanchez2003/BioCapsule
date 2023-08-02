87b753bac7335228c3f048befa53cbbe56d997fd
= <=>
2023-08-02 18:23:32 -0400

    beginning code for using single or multiple reference subjects when biocapsule is usde

---
+++ continuous_authentication/get_reference_subjects.py
@@ -0,0 +1,24 @@
+"""
+    BioCapsule
+    Get Reference Subjects
+
+    This module contains functions used to generate feature vectors 
+    for reference subjects for running tests. While this code will
+    work for any files organized in the same structure, this was made 
+    to be used with the LFW dataset.
+"""
+
+import tools
+
+def main():
+    pass
+
+
+# gets 
+def generate_rs_features(images_dir:str):
+
+    
+
+
+if __name__ == "__main__":
+    main()
